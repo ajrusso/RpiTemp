42c6477964b03ccb6943c781842e7b208d1334ad
root <root@DigiSancContDev.(none)>
2015-07-28 02:27:23 +0000

    added settings file and updated the heat_data file to leverage a json serialized objects.

--- heat_test.py
+++ heat_test.py
@@ -1,7 +1,11 @@
-import subprocess
 import plotly.plotly as py
 from plotly.graph_objs import *
 import plotly.tools as tls
+from datetime import datetime
+import subprocess
+import settings
+import time
+import json
 
 
 class RPiHeat:
@@ -9,25 +13,34 @@ class RPiHeat:
     def __init__(self):
         pass
 
-    def plotly_heat_report(self, traces, username, api_key):
-        tls.set_credentials_file(username=username,
-                                 api_key=api_key)
+    def plotly_heat_report(self, traces):
+        tls.set_credentials_file(username=settings.username,
+                                 api_key=settings.api_key)
         data = Data(traces)
         plot_url = py.plot(data)
         return plot_url
 
-    def build_trace(self, x, y):
+    def _build_trace(self, x, y):
         trace = Scatter(x = x,
                         y = y)
         return trace
 
+    def get_temp_data(self):
+        f = open('heat_data.txt', 'r')
+        heat_data = json.load(f)
+        f.close()
+        return heat_data 
+
     def record_heat(self, heat_data):
-        fo = open('heat_data.txt', 'a')
-        fo.write('%s,' %(heat_data))
-        fo.close()
+        temp_data = self.get_temp_data()
+        f = open('heat_data.txt', 'w')
+        utcnow = str(datetime.utcnow())
+        temp_data['heat_data'].append((heat_data, utcnow))
+        json.dump(temp_data, f)
+        f.close()
 
     def get_temp_c(self):
-        temp_c = subprocess.check_output(["vcgencmd", "measure_temp"]).split("'")[0].split("=")[1]
+        temp_c = float(subprocess.check_output(["vcgencmd", "measure_temp"]).split("'")[0].split("=")[1])
         return temp_c
 
     def c_2_f(self, temp_c):
@@ -36,9 +49,9 @@ class RPiHeat:
 
 
 if __name__ == "__main__":
-    rpi_heat = RPiHeat()
-
-    temp_c = rpi_heat.get_temp_c()
-    temp_f = rpi_heat.c_2_f(temp_c)
-
-    rpi_heat.record_heat(temp_f)
+    while True:
+        rpi_heat = RPiHeat()
+        temp_c = rpi_heat.get_temp_c()
+        temp_f = rpi_heat.c_2_f(temp_c)
+        rpi_heat.record_heat(temp_f)
+        time.sleep(60)
